7eea130fd38d2554f80e970847136aa28d2c1113
king <369558172@qq.com>
2018-04-07 22:23:30 +0800

    FINISH PRACTICE09

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,38 @@
 package practice09;
 
 public class Klass {
+    private int number;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public String getDisplayName() {
+        return "Class " + this.number;
+    }
+
+    public void assignLeader(Student student) {
+        if (student.getKlass().number != this.number) {
+            System.out.print("It is not one of us.\n");
+        } else {
+            this.leader = student;
+        }
+    }
+
+    public void appendMember(Student student) {
+        student.setKlass(this);
+    }
 }
--- src/main/java/practice09/Person.java
+++ src/main/java/practice09/Person.java
@@ -1,4 +1,69 @@
 package practice09;
 
 public class Person {
+
+    private String name;
+    private int age;
+
+    private int id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person(int id, String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        return this.name.hashCode() + this.getId();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof Person)) {
+            return false;
+        }
+        final Person person = (Person) obj;
+        return this.getId() == person.getId();
+    }
+
+    public String introduce() {
+        return "My name is " + this.name + ". I am " + age + " years old.";
+    }
+
 }
