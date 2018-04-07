45c1a24242a1b5ae76eb4f3e6bfa903e0fa71334
king <369558172@qq.com>
2018-04-07 20:47:56 +0800

    FINISH PRACTICE06

--- src/main/java/practice06/Person.java
+++ src/main/java/practice06/Person.java
@@ -1,4 +1,32 @@
 package practice06;
 
 public class Person {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String introduce() {
+        return "My name is " + this.name + ". I am " + age + " years old.";
+    }
+
 }
--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,30 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+
+    private Integer klass;
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+        this.klass = null;
+    }
+
+    public Teacher(String name, int age, Integer klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        StringBuilder s = new StringBuilder(super.introduce() + " I am a Teacher. I teach");
+        return klass == null ? s.append(" No Class.").toString() : s.append(" Class ").append(this.klass).append(".").toString();
+    }
 }
