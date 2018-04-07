bf9671f9a1454c14d4df4aa522d7233346248fe4
king <369558172@qq.com>
2018-04-07 17:49:10 +0800

    FINISH PRACTICE01

--- src/main/java/practice01/Person.java
+++ src/main/java/practice01/Person.java
@@ -1,4 +1,33 @@
 package practice01;
 
 public class Person {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String introduce() {
+        StringBuffer s = new StringBuffer();
+        s.append("My name is ").append(this.name).append(". I am ").append(age).append(" years old.");
+        return s.toString();
+    }
 }
